73dbf85e34f944bd08f069aa42bb102ad5c1fae8
insitewebcom <31064713+insitewebcom@users.noreply.github.com>
2017-08-21 22:49:19 +0800

    БОЛЬШЕАНИМАЦИИ

--- js/common.js
+++ js/common.js
@@ -116,6 +116,9 @@ function show() {
             $('.right').animated('fadeInRight');
             $('.person:nth-child(even)').animated('fadeInLeft');
             $('.person:nth-child(odd)').animated('fadeInRight');
+            $('.workExapmle h2').animated('fadeInDown');
+            $('.information p').animated('fadeInRight');
+            $('.flip-container').animated('fadeInLeft');
 
         });
 }
@@ -139,6 +142,9 @@ function hide(speedTopScroll) {$('.close').css('display', 'none');
            $('.right').removeClass('animated').removeClass('fadeInRight').css('opacity', '0');
              $('.person:nth-child(even)').removeClass('animated').removeClass('fadeInLeft').css('opacity', '0');
              $('.person:nth-child(odd)').removeClass('animated').removeClass('fadeInRight').css('opacity', '0');
+             $('.information p').removeClass('animated').removeClass('fadeInRight').css('opacity', '0');
+             $('.flip-container').removeClass('animated').removeClass('fadeInLeft').css('opacity', '0');
+            $('.workExapmle h2').removeClass('animated').removeClass('fadeInLeft').css('opacity', '0');
             $(".downButton").css("display", "none"); 
 
         }, 110);  
