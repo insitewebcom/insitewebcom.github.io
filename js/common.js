aae11aa6434c2e1521a70d2068be41834ee7ad86
insitewebcom <31064713+insitewebcom@users.noreply.github.com>
2017-08-22 08:24:35 +0800

    2

--- js/common.js
+++ js/common.js
@@ -330,7 +330,7 @@ $(".inputContainer").submit(function() { //Change
         var th = $(this);
         $.ajax({
             type: "POST",
-            url: "mail.php", //Change
+            url: "../mail.php", //Change
             data: th.serialize()
         }).done(function() {
             alert("Thank you!");
